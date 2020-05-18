1595d80401156d1ba3b3e0d5ba1fb8cbd162405d
Juan (programming) <programjuan@Juans-MacBook-Pro.local>
2020-05-18 19:13:18 -0400

    Moved async call to fetchShow.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
 import Dropdown from "react-dropdown";
 import parse from "html-react-parser";
 
 import { formatSeasons } from "./utils/formatSeasons";
+import {fetchShow } from './api/fetchShow';
 
 import Episodes from "./components/Episodes";
 import "./styles.css";
+
+const url = "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
+
 
 export default function App() {
   const [show, setShow] = useState(null);
@@ -14,19 +17,28 @@ export default function App() {
   const [selectedSeason, setSelectedSeason] = useState("");
   const episodes = seasons[selectedSeason] || [];
 
+  // useEffect(() => {
+  //   export const fetchShow = () => {
+  //     axios
+  //       .get(
+  //         "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
+  //       )
+  //       .then(res => {
+  //         setShow(res.data);
+  //         setSeasons(formatSeasons(res.data._embedded.episodes));
+  //       });
+  //   };
+  //   fetchShow();
+  // }, []);
+
   useEffect(() => {
-    const fetchShow = () => {
-      axios
-        .get(
-          "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
-        )
-        .then(res => {
-          setShow(res.data);
-          setSeasons(formatSeasons(res.data._embedded.episodes));
-        });
-    };
-    fetchShow();
+    fetchShow(url)
+      .then(res => {
+        setShow(res.data)
+        setSeasons(formatSeasons(res.data._embedded.episodes))
+      })
   }, []);
+  
 
   const handleSelect = e => {
     setSelectedSeason(e.value);
